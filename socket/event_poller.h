463b47220c26de8289a333231db8a760d144bdfc
jiafie <67643874+jiafie7@users.noreply.github.com>
2025-01-03 20:59:00 +0800

    commit EventPoller

---
+++ socket/event_poller.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <sys/epoll.h>
+
+namespace melon
+{
+  namespace socket
+  {
+    class EventPoller
+    {
+    public:
+      EventPoller();
+      ~EventPoller();
+
+      bool create(int max_conn);
+      void addFd(int fd, uint32_t events);
+      void modFd(int fd, uint32_t events);
+      void delFd(int fd);
+
+      int wait(int timeout);
+
+      bool isSet(int idx, uint32_t events);
+      void getFd(int idx);
+
+    private:
+      int m_epoll_fd;
+      int m_max_conn;
+      struct epoll_event* m_events;
+    };
+  }
+}
